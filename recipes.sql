b78a66613806c68273deb04613ff8e5d734f2787
Simon Cheek <simon.cheek@yahoo.com>
2024-10-01 20:42:16 -0600

    Finish table setup

--- recipes.sql
+++ recipes.sql
@@ -1,21 +1,57 @@
-DROP SCHEMA IF EXISTS 'recipes-app';
+DROP SCHEMA IF EXISTS `recipes_app`;
 
-CREATE SCHEMA 'recipes_app';
+CREATE SCHEMA `recipes_app`;
 
-USE 'recipes_app';
+USE `recipes_app`;
 
-CREATE TABLE 'users' (
-  'id' int NOT NULL AUTO_INCREMENT,
-  'username' varchar(64) DEFAULT NULL,
-  'description' TEXT DEFAULT NULL,
-  PRIMARY KEY ('id')
-) AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+-- Table for storing users
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) DEFAULT NULL,
+  `description` TEXT DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-CREATE TABLE 'recipes' (
-  'id' int NOT NULL AUTO_INCREMENT,
-  'name' varchar(64) DEFAULT NULL,
-  ''
-);
+-- Table for storing recipes
+CREATE TABLE `recipes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `description` TEXT DEFAULT NULL,
+  `user_id` int,  -- Foreign key to users
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_user_recipes`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-CREATE TABLE 'categories' ();
+-- Table for storing categories
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `description` TEXT DEFAULT NULL,
+  `user_id` int,  -- Foreign key to users
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_user_categories`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `users` (`id`)
+    ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
+-- Junction table for recipes and categories
+CREATE TABLE `recipe_category` (
+  `recipe_id` int NOT NULL,
+  `category_id` int NOT NULL,
+  PRIMARY KEY (`recipe_id`, `category_id`),
+  CONSTRAINT `fk_recipe`
+    FOREIGN KEY (`recipe_id`)
+    REFERENCES `recipes` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_category`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `categories` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
