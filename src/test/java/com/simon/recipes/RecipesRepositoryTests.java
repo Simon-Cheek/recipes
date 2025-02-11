b9e3c14cc7e8fa166782c8c7a77a409a5fb1b1a9
Simon-Cheek <simon.cheek@yahoo.com>
2025-02-10 20:26:39 -0700

    Remove unnecessary imports

--- src/test/java/com/simon/recipes/RecipesRepositoryTests.java
+++ src/test/java/com/simon/recipes/RecipesRepositoryTests.java
@@ -1,9 +1,7 @@
 package com.simon.recipes;
 
-import com.simon.recipes.dto.UserInfo;
 import com.simon.recipes.entity.User;
 import com.simon.recipes.repository.UserRepository;
-import com.simon.recipes.service.RecipesServiceImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
